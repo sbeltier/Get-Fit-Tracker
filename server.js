7849615d3c59e998515602c633c5610ff047d5ed
sbeltier <sarahnbeltier@gmail.com>
2021-10-14 23:31:11 -0700

    update connection

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useFindAndModify: true
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // Routes
